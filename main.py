e7889a3de86855680cc33204139c293ab5435522
Erik Madson <erik.madson@gmail.com>
2025-07-21 21:05:26 -0500

    CH2 L3

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     screen = pygame.display.set_mode(
         (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
     )
@@ -14,7 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
